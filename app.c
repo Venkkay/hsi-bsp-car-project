39543b02b75d0e11970eebeeef892226335beb0b
gastonks <rombar999@gmail.com>
2024-12-20 09:44:57 +0100

    use define variable

--- app.c
+++ app.c
@@ -9,19 +9,19 @@ int main() {
     uint8_t udp_frame[DRV_UDP_100MS_FRAME_SIZE];
 
     drv_fd = drv_open();
-    if (drv_fd == -1) {
+    if (drv_fd == DRV_ERROR) {
         printf("Open connection with driver failed : %s...\n", strerror(errno));
         return DRV_ERROR;
-    } else if (drv_fd == -2) {
+    } else if (drv_fd == DRV_VER_MISMATCH) {
         printf("Version of API is not compatible with driver : %s...\n", strerror(errno));
         return DRV_VER_MISMATCH;
     }
 
-    printf("Lectures des trames UDP...\n");
+    printf("Reading UDP frame...\n");
 
     while (1) {
         drv_frame = drv_read_udp_100ms(drv_fd, udp_frame);
-        if (drv_frame != 0) {
+        if (drv_frame != DRV_SUCCESS) {
             printf("UDP frame read failed : %s...\n", strerror(errno));
             break;
         }
